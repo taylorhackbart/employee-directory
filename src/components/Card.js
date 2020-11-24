8844a1cd4e4227508d5cd114752509ed3bef57e9
Taylor <taylor.hackbart@du.edu>
2020-11-24 14:09:48 -0800

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./style.css";
 function Card(props) {
   return (
     <table>
@@ -7,26 +7,29 @@ function Card(props) {
         <tr>
           <th></th>
           <th onClick={() => props.employeeArray()}>Name</th>
-          <th>Cell Number: </th>
           <th>Email: </th>
+          <th>Cell: </th>
           <th>Location: </th>
         </tr>
       </thead>
       <tbody className="list-group">
-        {props.employees.map(({ name, cell, email, login, picture, location }) => {
-          if (name.first.includes(props.search)) {
-            return (
-              <tr className="list-group-item" key={login.uuid}>
-                <td>
-                  <img src={picture.thumbnail} alt="" />
-                </td>
-                <td>{name.first} {name.last}</td>
-                <td>{email}</td>
-                <td>{cell}</td>
-                <td>{location.city}, {location.state}</td>
-              </tr>
-            )}
-        })}
+        {props.employees.map(
+          ({ name, cell, email, login, picture, location }) => {
+            if (name.last.includes(props.search)) {
+              return (
+                <tr className="list-group-item" key={login.uuid}>
+                  <td>
+                  <img src={picture.medium} alt="" />
+                  </td>
+                  <td>{name.last}, {name.first}</td>
+                  <td>{email}</td>
+                  <td>{cell}</td>
+                  <td>{location.city}, {location.state}</td>
+                </tr>
+              );
+            }
+          }
+        )}
       </tbody>
     </table>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import SearchForm from "./SearchForm"
-import Card from "./Card"
-
-
+import SearchForm from "./SearchForm";
+import Card from "./Card";
+import "./style.css";
 
 class Search extends Component {
   state = {
     employees: [],
     search: "",
+    sort: "ascend"
   };
 
   componentDidMount() {
@@ -16,46 +16,47 @@ class Search extends Component {
   }
   searchEmployee = () => {
     API.search()
-    .then((res) => this.setState({employees: res.data.results}))
-    .catch(err => console.log(err))
-    }
+      .then((res) => this.setState({ employees: res.data.results }))
+      .catch((err) => console.log(err));
+  };
 
   handleInputChange = (event) => {
-    console.log(event.target)
+    // console.log(event.target);
     const search = event.target.name;
     const value = event.target.value;
-    this.setState({[search]: value});
+    this.setState({ [search]: value });
   };
- employeeArray = () => {
-    const newArr = [...this.state.employees]
+  employeeArray = () => {
+    const newArr = [...this.state.employees].sort((a,b) => a.name.last > b.name.last ? 1 : -1)
     this.setState({
-      ...this.state, employees: newArr
-    }) 
+      ...this.state,
+      employees: newArr,
+    });
   };
 
   render() {
-    console.log(this.state.employees)
-    return ( 
+    // console.log(this.state.employees);
+    return (
       <div>
-      
         <h1>List of Employees</h1>
         <h3>To search for an employee, enter their name below:</h3>
-        <SearchForm
-        search={this.state.search}
-        handleInputChange = {this.handleInputChange}
-         />
-         <Card
-         employees = {this.state.employees}
-         search= {this.state.search}
-         employeeArray = {this.employeeArray}
+        <div className="searchForm">
+          <SearchForm
+            search={this.state.search}
+            handleInputChange={this.handleInputChange}
           />
-
-
-   
+          </div>
+          <div className = "forms">
+          <Card
+            employees={this.state.employees}
+            search={this.state.search}
+            employeeArray={this.employeeArray}
+            sort = {this.state.sort}
+          />
+        </div>
       </div>
     );
   }
 }
 
 export default Search;
-
